2787dbdb869bea10b41f15f4f275a519854e6cf6
kamalakannanngk <111722165+kamalakannanngk@users.noreply.github.com>
2023-05-09 16:29:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from email_validator import validate_email, EmailNotValidError
 from datetime import datetime
 import os
+from PIL import Image
 
 # Doctor appointment system
 st.set_page_config(page_title="Doctor Appointment System", page_icon=":hospital:", layout="wide")
@@ -55,7 +56,7 @@ with tab1:
                 st.warning("Invalid email address")
 
         # Doctor
-        doctor = st.selectbox("Doctor", ["Dr. P.Arumugam", "Dr. R.Thangaraj", "Dr. P.Sivakumar"])
+        doctor = st.selectbox("Doctor", ["Dr. S. Sivakarthikeyan", "Dr. R.Thangaraj", "Dr. R.Pavithra"])
 
         c1,c2 = st.columns(2)
 
@@ -65,7 +66,7 @@ with tab1:
         with c2:
             # Time
             valid_times = []
-            for hour in range(24):
+            for hour in range(8, 17):
                 for minute in ['00', '30']:
                     time = f"{hour:02d}:{minute}"
                     valid_times.append(time)
@@ -112,4 +113,123 @@ with tab1:
                     c.success("Appointment Successful")
 
 with tab2:
-    st.table(df)
+    st.table(df.drop("Email",axis=1))
+    st.button("Refresh")
+
+with tab3:
+
+    column1, column2, column3 = st.columns([1,3,1])
+
+    with column2:
+        st.write("Fill the particulars to cancel your appointment!")
+        #st.set_page_config(layout = 'center')
+        k = st.empty()
+        # Name
+        name_ = st.text_input("Enter your name")
+        
+        # Email
+        email_ = st.text_input("Enter your email")
+
+        # Doctor
+        doctor_ = st.selectbox("Select Doctor", ["Dr. P.Arumugam", "Dr. R.Thangaraj", "Dr. P.Sivakumar"])
+
+        col1,col2 = st.columns(2)
+
+        with col1:
+            # Date
+            date_ = st.date_input("Select Date")
+        with col2:
+            # Time
+            valid_times_ = []
+            for hour in range(8, 17):
+                for minute in ['00', '30']:
+                    time_ = f"{hour:02d}:{minute}"
+                    valid_times_.append(time_)
+
+            # Display dropdown select box
+            selected_time = st.selectbox("Select Time", valid_times)
+
+            # Print selected time
+            st.write("Selected time:", selected_time)
+        if st.button("Remove"):
+            if email_ != "" and date_ != "" and time_ != "":
+                d1=df[email_ == df["Email"]]
+                df = df.drop(df[email_ == df["Email"]].index)
+                df.to_csv('data.csv', index = False)
+                st.success("Appointment cancelled")
+            else:
+                k.error("Please enter valid Email, Date and Time!")
+
+
+with tab4:
+
+    image_1 = Image.open("D:\\NGK\\Doctor Appoinment System\\DOc_appointment-main\\doctor_1.jfif")
+    image_2 = Image.open("D:\\NGK\\Doctor Appoinment System\\DOc_appointment-main\\doctor_2.jfif")
+    image_3 = Image.open("D:\\NGK\\Doctor Appoinment System\\DOc_appointment-main\\doctor__3.jfif")
+    
+    #resize_1 = image_1.resize(200,200)
+
+
+    with st.container():
+        img_col_1, img_col_2 = st.columns([1,4])
+        with img_col_1:
+            st.image(image_1, caption = "Dr.S.Sivakarthikeyan")
+        with img_col_2:
+            st.header("CARDIOLOGY SPECIALIST")
+            st.write("""
+                    Education:
+
+Bachelor of Medicine and Bachelor of Surgery (MBBS)
+Master of Science in Cardiology
+Doctor of Medicine (MD) in Cardiology
+Specializations:
+
+Cardiac Imaging
+Heart Failure
+Coronary Artery Disease
+Arrhythmias
+Interventional Cardiology
+Experience:
+
+10 years of experience in Cardiology
+                """)
+
+    with st.container():
+        img_col_1, img_col_2 = st.columns([1,4])
+        with img_col_1:
+            st.image(image_2, caption = "Dr.R.Thangaraj")
+        with img_col_2:
+            st.header("NEUROLOGY SPECIALIST")
+            st.write("""
+                    Education:
+
+Bachelor of Science, Biology: University of California, Los Angeles
+Doctor of Medicine: University of Southern California, Keck School of Medicine
+Neurology Residency: Stanford University Medical Center
+Certifications and Memberships:
+
+American Board of Psychiatry and Neurology Certification in Neurology
+American Academy of Neurology Membership
+California Medical Association Membership
+                """)
+
+
+    with st.container():
+        img_col_1, img_col_2 = st.columns([1,4])
+        with img_col_1:
+            st.image(image_3, caption = "Dr.R.Pavithra")
+        with img_col_2:
+            st.header("DERMATOLOGY SPECIALIST")
+            st.write("""
+                    Qualifications: MD, FAAD\n
+Specializations: Dermatology, Cosmetic Dermatology\n
+Years of experience: 12 years
+Board certifications: American Board of Dermatology
+Education:
+
+Bachelor's degree in Biology, University of California, Los Angeles (UCLA)
+Doctor of Medicine (MD), Keck School of Medicine of USC
+Residency: Dermatology, Keck School of Medicine of USC
+Fellowship: Cosmetic Dermatology, UCLA School of Medicine
+Professional memberships: American Academy of Dermatology, American Society for Dermatologic Surgery, Women's Dermatologic Society
+                """)
